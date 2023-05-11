988288b9daec501bca796ea8a9453253dcfe046c
juandapps <juandapps@hotmail.com>
2023-05-11 10:08:43 -0500

    ➕Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+menu_item_1 = SecretMenuItem.create(
+  menu_name: "Wok Pork",
+  restaurant_name: "Noah",
+  menu_description: "Tender slices of stir-fried pork cooked with vibrant vegetables. Served with steamed rice or as
+a sushi roll filling."
+  )
+
+menu_item_2 = SecretMenuItem.create(
+  menu_name: "Chia Sur Rice",
+  restaurant_name: "Noah",
+  menu_description: " Fluffy steamed rice cooked with a medley of vibrant vegetables, succulent pieces of tender
+chicken or shrimp, and a harmonious blend of traditional Asian spices.",
+)
+
+menu_item_3 = SecretMenuItem.create(
+  menu_name: "T-Rex",
+  restaurant_name: "Imperial",
+  menu_description: "Tender cuts of prime beef, lamb, or chicken, expertly seasoned with a blend of aromatic spices
+that create a symphony of flavors. Served alongside a selection of grilled vegetables and accompanied by our signature sauces",
+)
